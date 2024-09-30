81d84ad3ddd9ffb8c41ee5fe2a582ba5183e6a60
Cleyson <37480421+cleyson-eng@users.noreply.github.com>
2024-09-30 20:56:29 -0300

    zlib fixes

--- lib2/zlib/1.2.13.ts
+++ lib2/zlib/1.2.13.ts
@@ -21,7 +21,7 @@ function(__self_inc)
 	#FINDABLE by boost
 	set(ZLIB_FOUND ON PARENT_SCOPE)
 	set(ZLIB_LIBRARY zlibstatic PARENT_SCOPE)
-	set(ZLIB_INCLUDE_DIR "\${CMAKE_CURRENT_LIST_DIR}/zlib-1.2.13" "\${CMAKE_CURRENT_BINARY_DIR}/zlib" PARENT_SCOPE)
+	set(ZLIB_INCLUDE_DIR "\${CMAKE_CURRENT_LIST_DIR}/zlib-1.2.13" "\${CMAKE_CURRENT_BINARY_DIR}/zlib" CACHE INTERNAL "ZLIB_INCLUDE_DIR")
 	add_library(ZLIB::ZLIB ALIAS zlibstatic)
 	set(BOOST_IOSTREAMS_ENABLE_ZLIB ON PARENT_SCOPE)
 endfunction()
@@ -45,6 +45,10 @@ export async function source(outRoot:string):Promise<Lib> {
 	 .cmake_stripReferences("minigzip64")
 	 .cmake_stripReferences("example64")
 	 .save();
+
+	//remove wrong symbol (error)
+	const f2 = new Inject(P.resolve(cmakeRoot, 'zlib.map'));
+	f2.replace(/(\s)*gz_intmax;/g, '').save();
 
 	writeTextFile(P.resolve(outRoot, 'zlib', 'inc.cmake'), inctxt, { ifdiff:true });
 
